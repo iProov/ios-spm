01e500316b0af95e891cba890eabfa6561c4d1f2
Emre Sari <emre.sari@iproov.com>
2025-01-07 16:40:13 +0000

    Release 11.0.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "iProov",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "iProov",
@@ -14,19 +14,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "iProov",
-            url: "https://github.com/iProov/ios/releases/download/10.3.1/iProov.xcframework.zip",
-            checksum: "a471a4887814c732f7342ac99f7688276803b3c388e273bd708e369840cb4073"
-        ),
-        .binaryTarget(
-            name: "Starscream",
-            url: "https://github.com/iProov/ios/releases/download/10.3.1/Starscream.xcframework.zip",
-            checksum: "2fe35875b529655237bb65fc902cc2cfdfa0362de4d1e5ccdcbbf36b4d6e88db"
+            url: "https://github.com/iProov/ios/releases/download/11.0.0/iProov.xcframework.zip",
+            checksum: "59cc77b08836ebfd70049271c6c6a841fa702021ea15670e3daa11f22acd9f29"
         ),
         .target(
             name: "iProovTargets",
             dependencies: [
-                .target(name: "iProov", condition: .when(platforms: .some([.iOS]))),
-                .target(name: "Starscream", condition: .when(platforms: .some([.iOS]))),
+                .target(name: "iProov", condition: .when(platforms: .some([.iOS])))
             ],
             path: "Sources",
             resources: [
