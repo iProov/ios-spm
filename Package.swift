d8291717c1cc8e12b84052807e37e5de46584bb5
Emre Sari <emre.sari@iproov.com>
2025-01-08 10:12:13 +0000

    Release 11.0.3

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "iProov",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "iProov",
@@ -14,13 +14,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "iProov",
-            url: "https://github.com/iProov/ios/releases/download/10.3.3/iProov.xcframework.zip",
-            checksum: "80ca7579fbe56faf13e0fdd72aa62c027d0b33754aa0cbe9e9389e5f4a2f550b"
+            url: "https://github.com/iProov/ios/releases/download/11.0.3/iProov.xcframework.zip",
+            checksum: "6da33ee7eb224083a0565d4d6a1663025e51d9677d536db93fe6ab8b39d783cf"
         ),
         .target(
             name: "iProovTargets",
             dependencies: [
-                .target(name: "iProov", condition: .when(platforms: .some([.iOS]))),
+                .target(name: "iProov", condition: .when(platforms: .some([.iOS])))
             ],
             path: "Sources",
             resources: [
