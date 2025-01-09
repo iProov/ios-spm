f19a10a199a44a5997359a179e7cfa7f0057c718
Emre Sari <emre.sari@iproov.com>
2025-01-09 11:21:05 +0000

    Change folder name

--- Package.swift
+++ Package.swift
@@ -4,25 +4,27 @@ import PackageDescription
 
 let package = Package(
     name: "iProov",
-    platforms: [.iOS(.v13)],
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
         .library(
             name: "iProov",
             targets: ["iProovTargets"]
-        )
+        ),
     ],
     targets: [
         .binaryTarget(
-            name: "iProov",
+            name: "iProov",            
             url: "https://github.com/iProov/ios/releases/download/12.2.1/iProov.xcframework.zip",
             checksum: "1959b3b13d86f96c53d5c82306e5ada2d79e8b49252d67336db70d4350d0a461"
         ),
         .target(
             name: "iProovTargets",
             dependencies: [
-                .target(name: "iProov", condition: .when(platforms: .some([.iOS])))
+                .target(name: "iProov", condition: .when(platforms: .some([.iOS]))),
             ],
-            path: "Sources",
+            path: "iProovTargets",
             resources: [
                 .copy("Resources/PrivacyInfo.xcprivacy")
             ]
