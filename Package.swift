d3a91a931530a7cc2f6584484291e363b0280d4d
iProov <support@iproov.com>
2025-11-17 12:02:33 +0000

    Release 13.1.0

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "iProov",
-            url: "https://github.com/iProov/ios/releases/download/13.0.0/iProov.xcframework.zip",
-            checksum: "e496410aa3c0c8bac21d4de58b381b6c175ceddad0f4a7bc6d3b5ca78cd14424"
+            url: "https://github.com/iProov/ios/releases/download/13.1.0/iProov.xcframework.zip",
+            checksum: "e55f10505dd4346ca7e266c2e60d804b933e1ec04c1958e1c97af7644acf52f1"
         ),
         .target(
             name: "iProovTargets",
